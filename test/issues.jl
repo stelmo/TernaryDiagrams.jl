c9516440854efe5994bffb10e08b5766e683bef3
thomvet <thvt@lundbeck.com>
2024-11-25 11:55:46 +0100

    incorporate @theogf review

--- test/issues.jl
+++ test/issues.jl
@@ -1,18 +1,12 @@
-using GLMakie
-using TernaryDiagrams
-
 #This testset checks that reported issues have been resolved; prevents regressions.
 
 #https://github.com/stelmo/TernaryDiagrams.jl/issues/22
-a = try 
-        using CairoMakie
-        using TernaryDiagrams
+using CairoMakie
+using TernaryDiagrams
+@test_nowarn begin         
         fig = Figure()
         ax = Axis(fig[1, 1])
         ternaryaxis!(ax)
         ternaryscatter!(ax, [0.2, 0.1], [0.2, 0.6], [0.6, 0.3])
         fig
-    catch e 
-        e
     end
- @test !isa(a, Exception)
